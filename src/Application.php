6f80edce18de4618b89e53be2604ff426b8cc219
Leonid Chernenko <leonid.chernenko.vrn@yandex.ru>
2019-10-05 17:41:37 +0300

    Сделал определение главной страницы

--- src/Application.php
+++ src/Application.php
@@ -6,6 +6,37 @@ class Application
 {
     public function run(): void
     {
-        echo "Hello, World!";
+        $response = $this->runAction();
+        echo $response;
+    }
+
+    private function runAction(): string
+    {
+        $route = $this->getRoute();
+        if ($route === '/') {
+            return $this->actionMainPage();
+        }
+        $this->setNotFoundHeader();
+        return $this->notFoundResponse();
+    }
+
+    private function notFoundResponse(): string
+    {
+        return 'Ой. Страница не найдена. Нам очень жаль :(';
+    }
+
+    private function getRoute(): string
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    private function actionMainPage()
+    {
+        return 'Главная страница';
+    }
+
+    private function setNotFoundHeader(): void
+    {
+        http_response_code(404);
     }
 }
