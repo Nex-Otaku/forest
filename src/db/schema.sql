3e0bce57ec4d78e5a1915ad83e3a7d4dad569e0d
Leonid Chernenko <leonid.chernenko.vrn@yandex.ru>
2019-10-10 20:35:43 +0300

    Схема для пользовательской таблицы

---
+++ src/db/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `user` (
+       `id` VARCHAR(36) NOT NULL,
+       `login` VARCHAR(255) NOT NULL,
+       `password_hash` VARCHAR(255) NOT NULL,
+       `first_name` VARCHAR(255) NOT NULL,
+       `last_name` VARCHAR(255) NOT NULL,
+       `full_name` VARCHAR(255) NOT NULL,
+       PRIMARY KEY (`id`),
+       UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4;
