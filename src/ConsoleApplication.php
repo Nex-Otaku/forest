87aab0c57e99c3e8f79dcc2b2f000b52e5e2a136
Leonid Chernenko <leonid.chernenko.vrn@yandex.ru>
2020-02-07 10:05:09 +0300

    Эксперименты с консолью

--- src/ConsoleApplication.php
+++ src/ConsoleApplication.php
@@ -32,7 +32,21 @@ class ConsoleApplication
     public function runAction(string $route, array $params = []): void
     {
         if ($route === 'hello') {
+            var_dump($params);
             echo "Hello World!\n";
+            return;
         }
+        if ($route === 'hello2') {
+
+            echo "Hello World2!\n";
+            return;
+        }
+        if ($route === 'kiss') {
+            $person = $params[0];
+            echo "You kissed {$person}\n";
+            return;
+        }
+
+        echo "Not found action: {$route}\n";
     }
 }
